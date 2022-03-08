d169c2cd4ee60a1d6ffd31c5ec2a2e49716b75ae
James Wienecke <james.a.wienecke@gmail.com>
2022-03-08 14:41:20 -0600

    Implementing basics to create the authentication/authorization systems.

--- src/main/java/com/filmsage/filmsage/repositories/ReviewRepository.java
+++ src/main/java/com/filmsage/filmsage/repositories/ReviewRepository.java
@@ -1,7 +1,8 @@
 package com.filmsage.filmsage.repositories;
 
+import com.filmsage.filmsage.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository {
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 
 }
