ae69c081c20aa42b97d5c5f01c6eb2ea88bb600b
Eva Brechum <Brechum.Eva@gmail.com>
2022-03-08 11:07:20 -0600

    creating model for collections table

---
+++ src/main/java/com/filmsage/filmsage/model/Collections.java
@@ -0,0 +1,51 @@
+package com.filmsage.filmsage.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "collections")
+public class Collections {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    public Collections() {
+    }
+
+    public Collections(long id, String title, Timestamp createdAt) {
+        this.id = id;
+        this.title = title;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
