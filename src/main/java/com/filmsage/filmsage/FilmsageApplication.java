99cd605dae153af3f7ea11486737a37d3b54432f
James Wienecke <james.a.wienecke@gmail.com>
2022-03-08 09:22:49 -0600

    Basic project setup.

---
+++ src/main/java/com/filmsage/filmsage/FilmsageApplication.java
@@ -0,0 +1,13 @@
+package com.filmsage.filmsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmsageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filmsage/filmsage/FilmsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmsage.filmsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
