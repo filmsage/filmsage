a1dc60e2fdb35069caa13042f11abe369bd2be69
Eva Brechum <Brechum.Eva@gmail.com>
2022-03-09 23:54:46 -0600

    implementing collections controller

--- src/main/java/com/filmsage/filmsage/controllers/CollectionController.java
+++ src/main/java/com/filmsage/filmsage/controllers/CollectionController.java
@@ -1,5 +1,6 @@
 package com.filmsage.filmsage.controllers;
 
+import com.filmsage.filmsage.models.Collection;
 import com.filmsage.filmsage.repositories.ReviewRepository;
 import com.filmsage.filmsage.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
@@ -17,7 +18,7 @@ public class CollectionController {
     @GetMapping("/collections")
     public String collections( Model model) {
         model.addAttribute("");
-        return "collections/index";
+        return "collections/home";//"collections/index";
     }
 
 //    to View an individual collection
@@ -27,11 +28,25 @@ public class CollectionController {
         return "collections/show";
     }
 
-//    view users collection
-//    @GetMapping("/collection/create")
-//    public String viewCollectionCreatedTime(Model model) {
-//        model.addAttribute();
-//
-//
-//    }
+//  view the form to create a new movie collection
+    @GetMapping("/collections/create")
+    public String createMovieCollection(Model model) {
+        model.addAttribute("newCollection", new Collection());
+        return "collections/create";
+    }
+
+
+    @PostMapping("/collections/{id}/edit")
+    public String submitMovieCollection(@ModelAttribute Collection submitCollection, @PathVariable long id) {
+//       Collection submitCollection = usersDao.getById(id);
+//       submitCollection.setTitle(title);
+//       submitCollection.setBody(body);
+       return "redirect:/collections";
+    }
+
+    @GetMapping("collections/{id}/delete")
+    public String deleteCollection(@PathVariable long id) {
+        usersDao.deleteById(id);
+        return "redirect/collections";
+    }
 }
