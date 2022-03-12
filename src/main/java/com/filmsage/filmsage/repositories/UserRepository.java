14f627461add824dcc3f37037a4968964bc3cff2
James Wienecke <james.a.wienecke@gmail.com>
2022-03-12 05:12:17 -0600

    Added new methods to query users table for user existing with a specific username or email.

--- src/main/java/com/filmsage/filmsage/repositories/UserRepository.java
+++ src/main/java/com/filmsage/filmsage/repositories/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    User findByEmail(String email);
+    boolean existsUserByUsername(String username);
+    boolean existsUserByEmail(String email);
 }
 
