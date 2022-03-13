02a4331967aa154f7c2f125f7d11288d8ff19bf0
James Wienecke <james.a.wienecke@gmail.com>
2022-03-12 19:12:08 -0600

    Altered details loader to be more robust and work with roles.

--- src/main/java/com/filmsage/filmsage/services/UserDetailsLoader.java
+++ src/main/java/com/filmsage/filmsage/services/UserDetailsLoader.java
@@ -1,28 +1,92 @@
 package com.filmsage.filmsage.services;
 
 import com.filmsage.filmsage.models.User;
-import com.filmsage.filmsage.models.UserWithRoles;
+//import com.filmsage.filmsage.models.UserWithRoles;
+import com.filmsage.filmsage.models.auth.Privilege;
+import com.filmsage.filmsage.models.auth.Role;
 import com.filmsage.filmsage.repositories.UserRepository;
+import com.filmsage.filmsage.repositories.auth.RoleRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@Service("userDetailsService")
+@Transactional
 public class UserDetailsLoader implements UserDetailsService {
-    private final UserRepository users;
+    private final UserRepository userDao;
+    private final RoleRepository roleDao;
 
-    public UserDetailsLoader(UserRepository users) {
-        this.users = users;
+    public UserDetailsLoader(UserRepository userDao, RoleRepository roleDao) {
+        this.userDao = userDao;
+        this.roleDao = roleDao;
     }
 
+    // Just a heads up: this method is called "loadUserByUsername" but we are actually using
+    // email instead. this method comes from UserDetailsService, so we can't change the name 😥
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = users.findByUsername(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userDao.findByEmail(email);
         if (user == null) {
-            throw new UsernameNotFoundException("No user found for " + username);
+            return new org.springframework.security.core.userdetails.User(
+                    " ", " ", true, true, true, true,
+                    getAuthorities(Arrays.asList(
+                            roleDao.findByName("ROLE_USER"))));
         }
 
-        return new UserWithRoles(user);
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                getAuthorities(user.getRoles()));
     }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
+
+        return getGrantedAuthorities(getPrivileges(roles));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String privilege : privileges) {
+            authorities.add(new SimpleGrantedAuthority(privilege));
+        }
+        return authorities;
+    }
+
+    private List<String> getPrivileges(Collection<Role> roles) {
+
+        List<String> privileges = new ArrayList<>();
+        List<Privilege> collection = new ArrayList<>();
+        for (Role role : roles) {
+            privileges.add(role.getName());
+            collection.addAll(role.getPrivileges());
+        }
+        for (Privilege item : collection) {
+            privileges.add(item.getName());
+        }
+        return privileges;
+    }
+
+//    OLD STUFF
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = users.findByUsername(username);
+//        if (user == null) {
+//            throw new UsernameNotFoundException("No user found for " + username);
+//        }
+//
+//        return new UserWithRoles(user);
+//    }
 }
