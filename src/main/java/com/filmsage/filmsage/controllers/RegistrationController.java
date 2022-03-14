f52888bf54c4de8a79ed4fde0163f558c7595a63
James Wienecke <james.a.wienecke@gmail.com>
2022-03-14 00:06:20 -0500

    Moved registration control pre-email verification over to new user service control.

--- src/main/java/com/filmsage/filmsage/controllers/RegistrationController.java
+++ src/main/java/com/filmsage/filmsage/controllers/RegistrationController.java
@@ -1,14 +1,23 @@
 package com.filmsage.filmsage.controllers;
 
+import com.filmsage.filmsage.event.event.OnRegistrationComplete;
 import com.filmsage.filmsage.models.User;
 import com.filmsage.filmsage.models.VerificationToken;
+import com.filmsage.filmsage.models.dto.UserDTO;
+import com.filmsage.filmsage.repositories.UserRepository;
 import com.filmsage.filmsage.services.IUserService;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -16,9 +25,13 @@ import java.util.Locale;
 public class RegistrationController {
 
     private IUserService service;
+    private UserRepository userDao;
+    ApplicationEventPublisher eventPublisher;
 
-    public RegistrationController(IUserService service) {
+    public RegistrationController(IUserService service, UserRepository userDao, ApplicationEventPublisher eventPublisher) {
         this.service = service;
+        this.userDao = userDao;
+        this.eventPublisher = eventPublisher;
     }
 
     @GetMapping("/registration-confirm")
@@ -44,5 +57,41 @@ public class RegistrationController {
         user.setEnabled(true);
         service.saveRegisteredUser(user);
         return "redirect:/login";
+    }
+
+
+    @GetMapping("/register")
+    public String showSignupForm(Model model) {
+        model.addAttribute("user", new UserDTO());
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String submitUserRegistration(@Valid @ModelAttribute("user") UserDTO userDTO, BindingResult bindingResult, Model model, HttpServletRequest request) {
+        // TODO: more custom validation annotations on UserDTO
+        if (bindingResult.hasErrors()) {
+            return "users/register";
+        }
+        if (userDao.existsUserByUsername(userDTO.getUsername())) {
+            // TODO: inform user that the username can't be used
+            return "users/register";
+        }
+        if (userDao.existsUserByEmail(userDTO.getEmail())) {
+            // TODO: inform user the email can't be used
+            return "users/register";
+        }
+
+        User registered = service.registerNewUserAccount(userDTO);
+        String url = request.getContextPath();
+        eventPublisher.publishEvent(new OnRegistrationComplete(registered, request.getLocale(), url));
+        // map the form details into a new User for our persistence layer
+//        User user = new User(
+//                userDTO.getEmail(),
+//                userDTO.getUsername(),
+//                passwordEncoder.encode(userDTO.getPassword()),
+//                new Timestamp(System.currentTimeMillis())
+//        );
+//        userDao.save(user);
+        return "users/register-email-confirmation";
     }
 }
