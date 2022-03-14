148bf61b6ec24b2c12151a6492babd7bc20e617e
James Wienecke <james.a.wienecke@gmail.com>
2022-03-14 00:05:46 -0500

    Moved actual registration control out of the users controller.

--- src/main/java/com/filmsage/filmsage/controllers/UserController.java
+++ src/main/java/com/filmsage/filmsage/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.filmsage.filmsage.controllers;
 import com.filmsage.filmsage.models.User;
 import com.filmsage.filmsage.models.dto.UserDTO;
 import com.filmsage.filmsage.repositories.UserRepository;
+import com.filmsage.filmsage.services.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,34 +25,4 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/register")
-    public String showSignupForm(Model model) {
-        model.addAttribute("user", new UserDTO());
-        return "users/register";
-    }
-
-    @PostMapping("/register")
-    public String saveUser(@Valid @ModelAttribute("user") UserDTO userDTO, BindingResult bindingResult, Model model) {
-        // TODO: more custom validation annotations on UserDTO
-        if (bindingResult.hasErrors()) {
-            return "users/register";
-        }
-        if (userDao.existsUserByUsername(userDTO.getUsername())) {
-            // TODO: inform user that the username can't be used
-            return "users/register";
-        }
-        if (userDao.existsUserByEmail(userDTO.getEmail())) {
-            // TODO: inform user the email can't be used
-            return "users/register";
-        }
-        // map the form details into a new User for our persistence layer
-        User user = new User(
-                userDTO.getEmail(),
-                userDTO.getUsername(),
-                passwordEncoder.encode(userDTO.getPassword()),
-                new Timestamp(System.currentTimeMillis())
-        );
-        userDao.save(user);
-        return "redirect:/login";
-    }
 }
