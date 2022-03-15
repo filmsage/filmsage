613481a01bfd7c0aec682938672d60fa27483f67
Eva Brechum <Brechum.Eva@gmail.com>
2022-03-15 10:34:23 -0500

    finished updating userprofile to display user reviews

--- src/main/java/com/filmsage/filmsage/controllers/UserProfileController.java
+++ src/main/java/com/filmsage/filmsage/controllers/UserProfileController.java
@@ -1,6 +1,7 @@
 package com.filmsage.filmsage.controllers;
 
 import com.filmsage.filmsage.models.MediaItem;
+import com.filmsage.filmsage.models.User;
 import com.filmsage.filmsage.models.UserContent;
 import com.filmsage.filmsage.models.auth.UserPrinciple;
 import com.filmsage.filmsage.repositories.ReviewRepository;
@@ -27,50 +28,20 @@ public class UserProfileController {
         this.omdbRequester = omdbRequester;
     }
 
-    //    Get the user's review on their profile page
-//    @GetMapping("/profile")
-//    public String viewUserReview(Model model, @PathVariable long id) {
-//        UserReview userReview = getUserReview();
-//        model.addAttribute("user", userContent);
-//        return "/profile/create";
-//    }
-    @GetMapping("/profile")
-    public String showUserProfileReview(Model model, @PathVariable String imdb ) {
+
+    @GetMapping("/profile/{userId}")
+    public String showUserProfileReview(Model model, @PathVariable String userId) {
 //        get the usersContent to link all the users content and reviews
-       UserContent userContent = getUserContent();
-       model.addAttribute("user", userContent);
-        model.addAttribute("userReview", userReview());
-        model.addAttribute("movie", omdbRequester.getMovie(imdb));
-        return "redirect:/profile";
+        long id = Long.parseLong(userId);
+        model.addAttribute("user", userDao.getById(id));
+//        get back a collection of items that the user reviewed
+        model.addAttribute("reviews", reviewDao.findAllByUserContent_Id(id));
+        return "profile/profile-page";
     }
-
-//    @PostMapping("profile/review/create")
-//    public String showUserReview(@ModelAttribute UserReview userReview, @PathVariable long id) {
-//        UserContent userContent = getUserContent();
-//        userReview.setUserContent(userContent);
-//        reviewDao.save(userReview);
-//        return "profile/create";
-//    }
-
-
-    @RequestMapping (value ="/profile/edit",
-            method = RequestMethod.GET,
-            params = "userId")
-    public String showUserReviewEdit(Model model,@PathVariable String imdb, @RequestParam long userId) {
-        model.addAttribute("userReview", reviewDao.getById(userId));
-        model.addAttribute("movie", omdbRequester.getMovie(imdb));
-        return "redirect:/profile";
-}
-
-//    @PostMapping("profile/reviews/edit")
-//    public String submitUserReviewEdit(@ModelAttribute UserReview UserReview, @PathVariable Strin imdb){
-//        userReview.setUserContent(getUserContent());
-//       userReview.setUserContent
-//    }
 
     private UserContent getUserContent() {
         UserPrinciple principle = (UserPrinciple) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-return userContentDao.findUserContentByUser(principle.getUser());
+        return userContentDao.findUserContentByUser(principle.getUser());
 
     }
 }
