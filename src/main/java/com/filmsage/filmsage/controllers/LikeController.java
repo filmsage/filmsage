e496ea6fd5b8ab45285d90028c397e8184d94093
James Wienecke <james.a.wienecke@gmail.com>
2022-03-17 17:47:18 -0500

    Wrote controller code needed to process input into the likes service.

---
+++ src/main/java/com/filmsage/filmsage/controllers/LikeController.java
@@ -0,0 +1,30 @@
+package com.filmsage.filmsage.controllers;
+
+import com.filmsage.filmsage.repositories.ReviewRepository;
+import com.filmsage.filmsage.services.LikesService;
+import com.filmsage.filmsage.services.UserContentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LikeController {
+    
+    private UserContentService userContentService;
+    private ReviewRepository reviewDao;
+    private LikesService likesService;
+
+    public LikeController(UserContentService userContentService, ReviewRepository reviewDao, LikesService likesService) {
+        this.userContentService = userContentService;
+        this.reviewDao = reviewDao;
+        this.likesService = likesService;
+    }
+
+    @GetMapping("/like")
+    public ResponseEntity<?> likeReview(@RequestParam long r) {
+        long result = likesService.likeReview(r);
+        return ResponseEntity.ok(result);
+    }
+}
