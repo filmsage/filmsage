860406c4683e139eab9860573fbccc23d837f235
James Wienecke <james.a.wienecke@gmail.com>
2022-03-16 23:51:59 -0500

    Added additional method to UserContentService that may be useful but is not right now.

--- src/main/java/com/filmsage/filmsage/services/UserContentService.java
+++ src/main/java/com/filmsage/filmsage/services/UserContentService.java
@@ -1,5 +1,6 @@
 package com.filmsage.filmsage.services;
 
+import com.filmsage.filmsage.models.User;
 import com.filmsage.filmsage.models.UserContent;
 import com.filmsage.filmsage.models.auth.UserPrinciple;
 import com.filmsage.filmsage.repositories.UserContentRepository;
@@ -11,11 +12,11 @@ import org.springframework.stereotype.Service;
 public class UserContentService {
 
     private UserContentRepository userContentDao;
-    private UserRepository userRepository;
+    private UserRepository userDao;
 
-    public UserContentService(UserContentRepository userContentDao, UserRepository userRepository) {
+    public UserContentService(UserContentRepository userContentDao, UserRepository userDao) {
         this.userContentDao = userContentDao;
-        this.userRepository = userRepository;
+        this.userDao = userDao;
     }
 
     // this method will get the currently authenticated user's content from the database
@@ -25,6 +26,11 @@ public class UserContentService {
         UserPrinciple principle = (UserPrinciple) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         // step 2: get the UserContent object which links to all that user's user-created content
         return userContentDao.findUserContentByUser(principle.getUser());
+    }
+
+    public User getUser() {
+        UserPrinciple principle = (UserPrinciple) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return principle.getUser();
     }
 
 }
