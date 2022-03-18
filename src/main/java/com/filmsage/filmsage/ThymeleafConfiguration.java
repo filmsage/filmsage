46df43e442604a6961fa44d3bf8a3c2adbeec833
Eva Brechum <Brechum.Eva@gmail.com>
2022-03-18 11:39:13 -0500

    fixed thymeleaf namespace issues for the sec attributes for user authenticated or not authenticated

--- src/main/java/com/filmsage/filmsage/ThymeleafConfiguration.java
+++ src/main/java/com/filmsage/filmsage/ThymeleafConfiguration.java
@@ -3,6 +3,7 @@ package com.filmsage.filmsage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @Configuration
 public class ThymeleafConfiguration {
@@ -10,4 +11,13 @@ public class ThymeleafConfiguration {
     public SpringSecurityDialect springSecurityDialect() {
         return new SpringSecurityDialect();
     }
+//    public SpringTemplateEngine templateEngine() {
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//        templateEngine.setTemplateResolver(templateResolver());
+//        templateEngine.addDialect(new TilesDialect());
+//        templateEngine.addDialect(new SpringSecurityDialect());
+//        return templateEngine;
+//    }
+
+
 }
